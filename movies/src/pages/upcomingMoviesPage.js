23d2698585c730664b4b3a378ddbe721ac26e7b5
Wu SongYu <wuwilliam2002@gmail.com>
2024-10-20 17:44:59 +0100

    New button for upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -3,7 +3,7 @@ import { getUPComingMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+import AddToWatchIcon from "../components/cardIcons/addToWatch";
 
 const UpcomingMoviesPage = (props) => {
 
@@ -29,7 +29,7 @@ const UpcomingMoviesPage = (props) => {
       title="Upcoming Movies"
       movies={movies}
       action={(movie) => {
-        // return <AddToFavoritesIcon movie={movie} />
+        return <AddToWatchIcon movie={movie} />
       }}
     />
   );
